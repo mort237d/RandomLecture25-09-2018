fbf932a9268b5ccf1c5efe3855ad42a29f4d67ba
Morten <morto1995@hotmail.com>
2018-09-25 12:43:09 +0200

    Add project files.

---
+++ RandomLecture25-09-2018/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomLecture25_09_2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
