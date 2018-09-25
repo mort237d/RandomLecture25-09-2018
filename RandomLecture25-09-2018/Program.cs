974f88faf56d43e5a57efaa65bc57d57e694c27a
Morten <morto1995@hotmail.com>
2018-09-25 12:56:45 +0200

    FizzBuzz method added

--- RandomLecture25-09-2018/Program.cs
+++ RandomLecture25-09-2018/Program.cs
@@ -10,6 +10,18 @@ namespace RandomLecture25_09_2018
     {
         static void Main(string[] args)
         {
+            FizzBuzz();
+        }
+
+        static void FizzBuzz()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 15 == 0) Console.Write("FizzBuzz, ");
+                else if (i % 3 == 0) Console.Write("Fizz, ");
+                else if (i % 5 == 0) Console.Write("Buzz, ");
+                else Console.Write(i + ", ");
+            }
         }
     }
 }
