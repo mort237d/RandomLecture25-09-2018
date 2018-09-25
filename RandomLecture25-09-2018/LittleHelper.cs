58444cef7d4f590bec1b02d0f9f3957dd0493ee2
Morten <morto1995@hotmail.com>
2018-09-25 13:31:20 +0200

    LittleHelper.cs tilføjet

---
+++ RandomLecture25-09-2018/LittleHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomLecture25_09_2018
+{
+    class LittleHelper
+    {
+        private int counter;
+        public bool startWithQ(string s)
+        {
+            counter++;
+            return s.ToLower().StartsWith("q");
+        }
+
+        public int getStringLength(string s)
+        {
+            counter++;
+            return s.Length;
+        }
+    }
+}
